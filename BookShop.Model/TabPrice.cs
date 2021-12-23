734f1571541ea05500e7efa2ef89c11dcdd6c0aa
Starinin Andrey <starininandrey@gmail.com>
2021-12-23 19:00:28 +0300

    Upgrade

---
+++ BookShop.Model/TabPrice.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+
+namespace BookShop.Model
+{
+    public partial class TabPrice
+    {
+        public int Id { get; set; }
+        public int IdEdition { get; set; }
+        public double Cost { get; set; }
+        public double Price { get; set; }
+
+        public virtual TabEdition IdEditionNavigation { get; set; }
+    }
+}
---
+++ BookShop.Model/TabPublishingHouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookShop.Model
+{
+    public sealed partial class TabPublishingHouse
+    {
+        public TabPublishingHouse()
+        {
+            TabEditions = new HashSet<TabEdition>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<TabEdition> TabEditions { get; set; }
+    }
+}
