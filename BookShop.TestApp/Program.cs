5d12d4e64d47df4b380c68686f3762c3aa6988be
Starinin Andrey <starininandrey@gmail.com>
2021-12-27 16:07:30 +0300

    Update test app

--- BookShop.TestApp/Program.cs
+++ BookShop.TestApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BookShop.Lib;
-using Microsoft.EntityFrameworkCore;
+using BookShop.Model;
 
 namespace BookShop.TestApp
 {
@@ -10,21 +11,65 @@ namespace BookShop.TestApp
         private static void Main()
         {
             using var db = BookShopDb.Init();
-
-            var authors = db.TabAuthors
-                .Include(a => a.TabBooks)
-                    .ThenInclude(b => b!.IdGenreNavigation)
-                .ToList();
-
-            // var authors = db.TabAuthors.ToList();
             
+            ShowAuthors(db.TabAuthors);
+            Console.Write("Введите номер автора: ");
+            var idAuthor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            
+            ShowGenres(db.TabGenres);
+            Console.Write("Введите номер жанра: ");
+            var idGenre = Convert.ToInt32(Console.ReadLine());
+
+            /*var books = (from book in db.TabBooks
+                join author in db.TabAuthors on book.IdAuthor equals author.Id
+                join genre in db.TabGenres on book.IdGenre equals genre.Id
+                where book.IdAuthor == idAuthor && book.IdGenre == idGenre
+                select book).ToList();
+            ShowBooks(books);*/
+            
+            var books2 = from book in db.TabBooks
+                join author in db.TabAuthors on book.IdAuthor equals author.Id
+                join genre in db.TabGenres on book.IdGenre equals genre.Id
+                where book.IdAuthor == idAuthor && book.IdGenre == idGenre
+                select book;
+            
+            ShowBooks(books2);
+
+            /*db.TabBooks.Add(new Book
+                {
+                    Title = "Воскресенье",
+                    IdAuthorNavigation = new Author
+                    {
+                        LastName = "Толстой",
+                        FirstName = "Лев"
+                    },
+                    IdGenre = 2
+                });
+            db.SaveChanges();*/
+        }
+
+        static void ShowAuthors(IEnumerable<Author> authors)
+        {
             foreach (var author in authors)
             {
                 Console.WriteLine($"{author.Id}: {author.LastName} {author.FirstName}");
-                foreach (var book in author.TabBooks)
-                {
-                    Console.WriteLine($"{book.Id}: {book.Title}, {book.IdGenreNavigation.Name}");
-                }
+            }
+        }
+
+        static void ShowGenres(IEnumerable<Genre> genres)
+        {
+            foreach (var genre in genres)
+            {
+                Console.WriteLine($"{genre.Id}: {genre.Name}");
+            }
+        }
+
+        static void ShowBooks(IEnumerable<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Id}: {book.Title}; {book.YearOfCreation}");
             }
         }
     }
