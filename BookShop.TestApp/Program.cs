c70541e311667be4ff2555c3cba3b40fe5021ca7
Starinin Andrey <starininandrey@gmail.com>
2021-12-27 14:05:11 +0300

    Init test proj

---
+++ BookShop.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.TestApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            using var db;
+        }
+    }
+}
