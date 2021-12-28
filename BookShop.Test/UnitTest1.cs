e1af3f89e4b8ca945736a26fb1dcab0c8f2082c5
Starinin Andrey <starininandrey@gmail.com>
2021-12-28 14:33:04 +0300

    Init unit test proj

---
+++ BookShop.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookShop.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
